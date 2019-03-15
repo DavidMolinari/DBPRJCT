2e76259b9871f9925672f285b31ac006273808be
utilisateur <utilisateur@classmob-ftlv09.univ-tln.fr>
2019-03-15 15:18:56 +0100

    test etudiants

--- core/etudiant/getEtudiants.php
+++ core/etudiant/getEtudiants.php
@@ -20,16 +20,16 @@ function displayTableUsers($etudiants){
         echo
             "<tr>
                 <td class='user_id'>".$etudiant->ID."</td>
-                <td class='user_login'>".$etudiant->prenom."</td>
-                <td class='user_password'>".$etudiant->nom."</td>
-                <td class='user_adresse'>".$etudiant->adresse."</td>
-                <td class='user_ville'>".$etudiant->ville."</td>
-                <td class='user_region'>".$etudiant->region."</td>
-                <td class='user_mobile'>".$etudiant->isMobile."</td>
-                <td class='user_num'>".$etudiant->num."</td>
-                <td class='user_description'>".$etudiant->description."</td>
-                <td class='user_avatar'>".$etudiant->avatar."</td>
-                <td class='user_isUser'>".$etudiant->idUser."</td>
+                <td class='user_login'>".$etudiant->PRENOM."</td>
+                <td class='user_password'>".$etudiant->NOM."</td>
+                <td class='user_adresse'>".$etudiant->ADRESSE."</td>
+                <td class='user_ville'>".$etudiant->VILLE."</td>
+                <td class='user_region'>".$etudiant->REGION."</td>
+                <td class='user_mobile'>".$etudiant->IS_MOBILE."</td>
+                <td class='user_num'>".$etudiant->NUM."</td>
+                <td class='user_description'>".$etudiant->DESCRIPTION."</td>
+                <td class='user_avatar'>".$etudiant->AVATAR."</td>
+                <td class='user_isUser'>".$etudiant->ID_USER."</td>
                 <td class='user_last_login'>TODO</td>
                 <td class='user_edit'><button data-id='".$etudiant->ID."' class='btn btn-success editUser'></button></td>
                 <td class='user_remove'><button data-id='".$etudiant->ID."' class='btn btn-danger removeUser'></button></td>
